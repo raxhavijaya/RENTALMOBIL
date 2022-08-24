72d8fccd8b2e8af818a92fc0e73a30c7f152ee87
raxhavijaya <raxhavijaya31@gmail.com>
2022-08-24 23:15:06 +0700

    selesai

--- src/components/UI/TopSection.jsx
+++ src/components/UI/TopSection.jsx
@@ -6,9 +6,9 @@ import mercedes from "../../assets/images/mercedes.png";
 
 const TopSection = (showSection) => {
   return (
-    <div className="topSection mb-5" disable={showSection}>
-      <Container>
-        <Row className="d-flex">
+    <div className="topSection" disable={showSection}>
+      <Container className="topContainer">
+        <Row className="d-flex align-items-center justify-content-between">
           <Col className="topLeft mb-3" lg="6" md="6" sm="6">
             <h1>Sewa & Rental Mobil Terbaik di kawasan (Lokasimu)</h1>
             <p>Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas terbaik dengan harga terjangkau. Selalu siap melayani kebutuhanmu untuk sewa mobil selama 24 jam</p>
@@ -17,7 +17,7 @@ const TopSection = (showSection) => {
             </Link>
           </Col>
 
-          <Col className="topRight mb-3" lg="6" md="6" sm="6">
+          <Col className="topRight" lg="6" md="6" sm="6">
             <Image src={mercedes} alt="Mercedes" className="mercedes img-fluid fluid" />
           </Col>
         </Row>
